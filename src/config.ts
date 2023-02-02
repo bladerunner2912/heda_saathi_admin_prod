d866c7a0e5ac2d0cc93e67272bae8d5b3152a9ef
bladerunner2912 <bladerunner2912@gmail.com>
2023-02-03 04:53:28 +0530

    Tweaking package.json starting script

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,8 @@
 import * as dotenv from "dotenv";
 dotenv.config({ path: __dirname + '/.env' });
 
-const MONGO_USERNAME = process.env.MONGO_USERNAME || "";
-const MONGO_PASSWORD = process.env.MONGO_PASSWORD || "";
+const MONGO_USERNAME = process.env.MONGO_USERNAME || "bladerunner";
+const MONGO_PASSWORD = process.env.MONGO_PASSWORD || "daisy2308";
 const MONGO_URL = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@devhedasaathi.pwxdhtj.mongodb.net/?retryWrites=true&w=majority`;
 
 const SERVER_PORT = process.env.SERVER_PORT
